3941613f2c5eea0f085b473d24a4274d5e49f38a
hyewoncc <hyewoncc@gmail.com>
2022-09-25 14:38:00 +0900

    refactor: try-with-resources를 이용한 예외처리

--- src/main/java/springbook/UserDao.java
+++ src/main/java/springbook/UserDao.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.NoSuchElementException;
 import javax.sql.DataSource;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Controller;
@@ -13,49 +14,39 @@ public class UserDao {
 
     private DataSource dataSource;
 
-    public void add(final User user) throws SQLException {
-        Connection connection = dataSource.getConnection();
+    public void add(final User user) {
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement statement = connection.prepareStatement(
+                     "insert into users(id, name, password) values(?, ?, ?)")) {
+            statement.setString(1, user.getId());
+            statement.setString(2, user.getName());
+            statement.setString(3, user.getPassword());
 
-        PreparedStatement statement = connection.prepareStatement(
-                "insert into users(id, name, password) values(?, ?, ?)"
-        );
-        statement.setString(1, user.getId());
-        statement.setString(2, user.getName());
-        statement.setString(3, user.getPassword());
-
-        statement.executeUpdate();
-
-        statement.close();
-        connection.close();
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     public User get(final String id) throws SQLException {
-        Connection connection = dataSource.getConnection();
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement statement = connection.prepareStatement(
+                     "select id, name, password from users where id = ?")) {
+            statement.setString(1, id);
 
-        PreparedStatement statement = connection.prepareStatement(
-                "select id, name, password from users where id = ?"
-        );
-        statement.setString(1, id);
+            try (ResultSet resultSet = statement.executeQuery()) {
+                resultSet.next();
 
-        ResultSet resultSet = statement.executeQuery();
-        User user = null;
+                User user = new User();
+                user.setId(resultSet.getString("id"));
+                user.setName(resultSet.getString("name"));
+                user.setPassword(resultSet.getString("password"));
 
-        if (resultSet.next()) {
-            user = new User();
-            user.setId(resultSet.getString("id"));
-            user.setName(resultSet.getString("name"));
-            user.setPassword(resultSet.getString("password"));
+                return user;
+            } catch (SQLException e) {
+                throw new EmptyResultDataAccessException(1);
+            }
         }
-
-        resultSet.close();
-        statement.close();
-        connection.close();
-
-        if (user == null) {
-            throw new EmptyResultDataAccessException(1);
-        }
-
-        return user;
     }
 
     public int getCount() throws SQLException {
