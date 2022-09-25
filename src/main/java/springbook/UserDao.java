3878aaa9522d153a617385a464376a5d0f3f1a66
hyewoncc <hyewoncc@gmail.com>
2022-09-25 17:01:21 +0900

    refactor: RowMapper 분리

--- src/main/java/springbook/UserDao.java
+++ src/main/java/springbook/UserDao.java
@@ -4,6 +4,7 @@ import java.sql.ResultSet;
 import java.util.List;
 import javax.sql.DataSource;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 
 public class UserDao {
 
@@ -12,6 +13,15 @@ public class UserDao {
     public UserDao(final DataSource dataSource) {
         this.jdbcTemplate = new JdbcTemplate(dataSource);
     }
+
+    private RowMapper<User> userRowMapper =
+            (final ResultSet resultSet, final int rowNum) -> {
+                final var user = new User();
+                user.setId(resultSet.getString("id"));
+                user.setName(resultSet.getString("name"));
+                user.setPassword(resultSet.getString("password"));
+                return user;
+            };
 
     public void add(final User user) {
         jdbcTemplate.update("insert into users(id, name, password) values (?, ?, ?)",
@@ -23,26 +33,12 @@ public class UserDao {
     }
 
     public List<User> getAll() {
-        return jdbcTemplate.query("select * from users order by id",
-                (final ResultSet resultSet, final int rowNum) -> {
-                    final var user = new User();
-                    user.setId(resultSet.getString("id"));
-                    user.setName(resultSet.getString("name"));
-                    user.setPassword(resultSet.getString("password"));
-                    return user;
-                });
+        return jdbcTemplate.query("select * from users order by id", userRowMapper);
     }
 
     public User get(final String id) {
         return jdbcTemplate.queryForObject("select * from users where id = ?",
-                new Object[]{id},
-                (final ResultSet resultSet, final int rowNum) -> {
-                    final var user = new User();
-                    user.setId(resultSet.getString("id"));
-                    user.setName(resultSet.getString("name"));
-                    user.setPassword(resultSet.getString("password"));
-                    return user;
-                });
+                new Object[]{id}, userRowMapper);
     }
 
     public int getCount() {
